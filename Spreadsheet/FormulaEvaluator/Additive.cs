8222adf5ffe100b4b09758db0bd3b3ce18ebaf9b
werignac <48932607+werignac@users.noreply.github.com>
2021-01-27 17:33:24 -0800

    Added Operations beginnings.

---
+++ Spreadsheet/FormulaEvaluator/Additive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormulaEvaluator
+{
+    abstract class Additive : Operator
+    {
+        override public void HandleStacks(Stack<int> values, Stack<Operator> operators)
+        {
+            if (operators.IsOnTop<Operator, Additive>())
+            {
+                if (values.Count < 2)
+                    throw new ArgumentException("Tried to perform operation, but operands were missing.");
+
+                int operand2 = values.Pop();
+                int operand1 = values.Pop();
+
+                values.Push(operators.Pop().DoOperation(operand1, operand2));
+            }
+
+            operators.Push(this);
+        }
+    }
+}
---
+++ Spreadsheet/FormulaEvaluator/Operator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormulaEvaluator
+{
+    abstract class Operator
+    {
+        public abstract bool IsOperator(string token);
+
+        public abstract int DoOperation(int v1, int v2);
+
+        public virtual void HandleStacks(Stack<int> values, Stack<Operator> operators)
+        {
+            operators.Push(this);
+        }
+    }
+}
