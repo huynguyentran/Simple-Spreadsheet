9f5de88a23e4ff662f1b46aff4aa37df05cf1eb3
werignac <48932607+werignac@users.noreply.github.com>
2021-01-27 17:33:10 -0800

    Added Stack Extentions.

---
+++ Spreadsheet/FormulaEvaluator/StackExtentions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FormulaEvaluator
+{
+    static class StackExtentions
+    {
+        public static bool IsOnTop<T,S>(this Stack<T> s) where S : T
+        {
+            if (s.Count < 1)
+                return false;
+            return s.Peek() is S;
+        }
+    }
+}
