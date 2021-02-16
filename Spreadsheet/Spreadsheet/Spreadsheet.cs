79de71ab78f0077f8b7b5898059cdd956b16ceb2
werignac <48932607+werignac@users.noreply.github.com>
2021-02-16 12:50:43 -0800

    PS4 Skeleton

---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,40 @@
+﻿using SpreadsheetUtilities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SS
+{
+    class Spreadsheet : AbstractSpreadsheet
+    {
+        public override object GetCellContents(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IList<string> SetCellContents(string name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
