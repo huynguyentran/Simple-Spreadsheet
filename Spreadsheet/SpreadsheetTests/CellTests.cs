4a3406ebd84a91e02c9d5f8c0dc3c772e057b38c
werignac <48932607+werignac@users.noreply.github.com>
2021-02-18 21:15:58 -0800

    Cell Tests.

---
+++ Spreadsheet/SpreadsheetTests/CellTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+using SpreadsheetUtilities;
+using System.Collections.Generic;
+using System;
+
+namespace SpreadsheetTests
+{
+    [TestClass]
+    public class CellTests
+    {
+        [TestMethod]
+        public void CorrectCasting()
+        {
+            Cell str = new Cell("string");
+            Cell doub = new Cell(400413d);
+
+            Assert.AreEqual("string", str.Contents);
+            Assert.IsTrue(str.CanCast(out string strCont));
+            Assert.AreEqual("str", strCont.Substring(0, 3));
+
+            Assert.AreEqual(400413d, doub.Contents);
+            Assert.IsTrue(doub.CanCast(out double doubCont));
+            Assert.AreEqual(400413d / 2, doubCont / 2);
+        }
+    }
+}
