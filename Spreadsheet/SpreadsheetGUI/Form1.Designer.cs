1908223c43974480f2be849cfa2250b0b4d17cd2
u1315096@eng.utah.edu <u1315096@eng.utah.edu>
2021-03-10 16:26:51 -0700

    Display the cell name.

--- Spreadsheet/SpreadsheetGUI/Form1.Designer.cs
+++ Spreadsheet/SpreadsheetGUI/Form1.Designer.cs
@@ -29,20 +29,68 @@ namespace SpreadsheetGUI
         /// </summary>
         private void InitializeComponent()
         {
+            this.cellNameBox = new System.Windows.Forms.TextBox();
+            this.cellValueBox = new System.Windows.Forms.TextBox();
+            this.cellContentBox = new System.Windows.Forms.TextBox();
+            this.spreadSheetPanel = new SS.SpreadsheetPanel();
             this.SuspendLayout();
+            // 
+            // cellNameBox
+            // 
+            this.cellNameBox.Enabled = false;
+            this.cellNameBox.Location = new System.Drawing.Point(13, 8);
+            this.cellNameBox.Name = "cellNameBox";
+            this.cellNameBox.Size = new System.Drawing.Size(100, 22);
+            this.cellNameBox.TabIndex = 1;
+           
+            // 
+            // cellValueBox
+            // 
+            this.cellValueBox.Enabled = false;
+            this.cellValueBox.Location = new System.Drawing.Point(147, 8);
+            this.cellValueBox.Name = "cellValueBox";
+            this.cellValueBox.Size = new System.Drawing.Size(100, 22);
+            this.cellValueBox.TabIndex = 2;
+            // 
+            // cellContentBox
+            // 
+            this.cellContentBox.Location = new System.Drawing.Point(295, 8);
+            this.cellContentBox.Name = "cellContentBox";
+            this.cellContentBox.Size = new System.Drawing.Size(100, 22);
+            this.cellContentBox.TabIndex = 3;
+            // 
+            // spreadSheetPanel
+            // 
+            this.spreadSheetPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.spreadSheetPanel.Location = new System.Drawing.Point(13, 46);
+            this.spreadSheetPanel.Name = "spreadSheetPanel";
+            this.spreadSheetPanel.Size = new System.Drawing.Size(868, 470);
+            this.spreadSheetPanel.TabIndex = 4;
+       
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(886, 520);
+            this.Controls.Add(this.spreadSheetPanel);
+            this.Controls.Add(this.cellContentBox);
+            this.Controls.Add(this.cellValueBox);
+            this.Controls.Add(this.cellNameBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox cellNameBox;
+        private System.Windows.Forms.TextBox cellValueBox;
+        private System.Windows.Forms.TextBox cellContentBox;
+        private SS.SpreadsheetPanel spreadSheetPanel;
     }
 }
 
--- Spreadsheet/SpreadsheetGUI/Form1.cs
+++ Spreadsheet/SpreadsheetGUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,19 @@ namespace SpreadsheetGUI
         public Form1()
         {
             InitializeComponent();
+            spreadSheetPanel.SetSelection(0, 0);
+            spreadSheetPanel.SelectionChanged += displaySelection;
         }
+
+        private void displaySelection(SpreadsheetPanel ss)
+        {
+            int row, col;
+            ss.GetSelection(out col, out row);
+            row = row + 1;
+            col = col + 65;
+            char c = (char)col;
+            cellNameBox.Text = c.ToString() + row ;
+        }
+
     }
 }
