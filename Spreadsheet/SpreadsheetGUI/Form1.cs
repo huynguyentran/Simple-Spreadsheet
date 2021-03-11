a0d281058447ea3ea64a94512dfa2601aa84ce21
werignac <48932607+werignac@users.noreply.github.com>
2021-03-11 14:59:34 -0800

    Update dependencies in spreadsheet panel and fixed formula display bug.

--- Spreadsheet/SpreadsheetGUI/Form1.cs
+++ Spreadsheet/SpreadsheetGUI/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using SS;
+using SpreadsheetUtilities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -36,19 +37,38 @@ namespace SpreadsheetGUI
         {
             int row, col;
             ss.GetSelection(out col, out row);
-            string cellName = ((char)(col + 65)).ToString() + (row + 1);
+            string cellName = GetNameOfCell(col, row);
+
             cellNameBox.Text = cellName;
-            cellValueBox.Text = spreadsheet.GetCellValue(cellName).ToString();
-            cellContentBox.Text = spreadsheet.GetCellContents(cellName).ToString();
+            UpdateTopCellVisual(cellName);
+            
             ss.SetValue(col, row, spreadsheet.GetCellValue(cellName).ToString());
         }
 
-        private string getNameOfCell(int col , int row)
+        private string GetNameOfCell(int col , int row)
         {    
             string cellName = ((char)(col + 65)).ToString() + (row + 1);
             return cellName;
         }
 
+        private (int, int) GetCellRowAndCol(string name)
+        {
+            int col = name[0] - 65;
+            int row = int.Parse(name.Substring(1)) - 1;
+            return (col, row);
+        }
+
+        private void UpdateTopCellVisual(string cellName)
+        {
+            cellValueBox.Text = spreadsheet.GetCellValue(cellName).ToString();
+
+            object contents = spreadsheet.GetCellContents(cellName);
+
+            if (contents is Formula f)
+                cellContentBox.Text = "=" + f.ToString();
+            else
+                cellContentBox.Text = contents.ToString();
+        }
 
         private void cellContentBox_KeyPress(object sender, KeyPressEventArgs e)
         {
@@ -56,11 +76,16 @@ namespace SpreadsheetGUI
             {
                 int row, col;
                 spreadSheetPanel.GetSelection(out col, out row);
-                spreadsheet.SetContentsOfCell(getNameOfCell(col, row), cellContentBox.Text);
-                string cellName = ((char)(col + 65)).ToString() + (row + 1);
-                cellValueBox.Text = spreadsheet.GetCellValue(cellName).ToString();
-                cellContentBox.Text = spreadsheet.GetCellContents(cellName).ToString();
-                spreadSheetPanel.SetValue(col, row, spreadsheet.GetCellValue(cellName).ToString());
+                string cellName = GetNameOfCell(col, row);
+                IList<string> dependencies = spreadsheet.SetContentsOfCell(cellName, cellContentBox.Text);
+
+                UpdateTopCellVisual(cellName);
+
+                foreach (string dependency in dependencies)
+                {
+                    (int, int) coordinates = GetCellRowAndCol(dependency);
+                    spreadSheetPanel.SetValue(coordinates.Item1, coordinates.Item2, spreadsheet.GetCellValue(dependency).ToString());
+                }
             }
            
         }
