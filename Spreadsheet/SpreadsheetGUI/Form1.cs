64b27f32f41830ccb907b81aea9867e42cc491f3
werignac <48932607+werignac@users.noreply.github.com>
2021-03-10 16:02:07 -0800

    Updating text box values and contents.

--- Spreadsheet/SpreadsheetGUI/Form1.cs
+++ Spreadsheet/SpreadsheetGUI/Form1.cs
@@ -8,26 +8,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace SpreadsheetGUI
 {
     public partial class Form1 : Form
     {
+        private AbstractSpreadsheet spreadsheet;
+
         public Form1()
         {
             InitializeComponent();
             spreadSheetPanel.SetSelection(0, 0);
+
+            spreadsheet = new Spreadsheet(IsCellName, s => s.ToUpper(), "ps6");
+
+            spreadsheet.SetContentsOfCell("A2", "This Is Some Contents");
+            spreadsheet.SetContentsOfCell("B3", "2.33");
+            spreadsheet.SetContentsOfCell("C4", "=B3 + 9");
+            spreadsheet.SetContentsOfCell("E5", "=1 / (1-1)");
+
             spreadSheetPanel.SelectionChanged += displaySelection;
+            displaySelection(spreadSheetPanel);
+        }
+
+        private bool IsCellName(string varName)
+        {
+            //Only looks for one letter at start.
+            return Regex.IsMatch(varName, @"[A-Z][\d]+");
         }
 
         private void displaySelection(SpreadsheetPanel ss)
         {
             int row, col;
             ss.GetSelection(out col, out row);
-            row = row + 1;
-            col = col + 65;
-            char c = (char)col;
-            cellNameBox.Text = c.ToString() + row ;
+
+            string cellName = ((char)(col + 65)).ToString() + (row + 1);
+            cellNameBox.Text = cellName;
+
+            cellValueBox.Text = spreadsheet.GetCellValue(cellName).ToString();
+
+            cellContentBox.Text = spreadsheet.GetCellContents(cellName).ToString();
+
+            ss.SetValue(col, row, spreadsheet.GetCellValue(cellName).ToString());
         }
 
     }
